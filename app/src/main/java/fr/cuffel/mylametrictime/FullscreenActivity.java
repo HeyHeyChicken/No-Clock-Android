a5332cafc40044a42570706ed5314564929a27ab
Antoine Duval <33424294+HeyHeyChicken@users.noreply.github.com>
2022-01-03 08:10:17 +0100

    Allows user to use without server

--- app/src/main/java/fr/cuffel/mylametrictime/FullscreenActivity.java
+++ app/src/main/java/fr/cuffel/mylametrictime/FullscreenActivity.java
@@ -1,31 +1,57 @@
 package fr.cuffel.mylametrictime;
 
 import android.annotation.SuppressLint;
-
+import android.webkit.JavascriptInterface;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowInsets;
 import android.view.WindowManager;
-import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.FrameLayout;
-
-import com.google.android.material.textfield.TextInputLayout;
-
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
 import fr.cuffel.mylametrictime.databinding.ActivityFullscreenBinding;
+import android.os.PowerManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class FullscreenActivity extends AppCompatActivity {
     /**
@@ -39,6 +65,59 @@ public class FullscreenActivity extends AppCompatActivity {
     private SharedPreferences sharedPref;
     private FrameLayout SettingsLayout;
     public WebView MyWebView;
+    private HttpServer mHttpServer;
+    private int WebServerPort = 5000;
+    private int Green = Color.parseColor("#FF4CAF50");
+    private Context MyContext;
+    private HttpHandler rootHandler = new HttpHandler() {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            if(httpExchange.getRequestMethod().equals("GET")){
+                File f = new File(getFilesDir().getAbsolutePath() + "/My-LaMetric-Time-main/public" + httpExchange.getRequestURI().toString());
+                FileInputStream fl = new FileInputStream(f);
+                byte[] arr = new byte[(int)f.length()];
+                fl.read(arr);
+                fl.close();
+
+                httpExchange.sendResponseHeaders(200, arr.length);//response code and length
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(arr);
+                os.close();
+            }
+            else if(httpExchange.getRequestMethod().equals("POST")){
+                String response = "Notification sended !";
+                httpExchange.sendResponseHeaders(200, response.length());//response code and length
+
+                // GET REQ BODY
+                InputStreamReader isr =  new InputStreamReader(httpExchange.getRequestBody() ,"utf-8");
+                BufferedReader br = new BufferedReader(isr);
+                int b;
+                StringBuilder buf = new StringBuilder();
+                while ((b = br.read()) != -1) {
+                    buf.append((char) b);
+                }
+                br.close();
+                isr.close();
+
+                try {
+                    JSONObject json = new JSONObject(buf.toString());
+
+                    MyWebView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            MyWebView.evaluateJavascript("APP.showNotification(JSON.parse(\"" + json.toString().replace("\"", "\\\"") + "\"));", null);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        }
+    };
 
     public class MyJavascriptInterface {
         private FullscreenActivity MyActivity;
@@ -58,6 +137,84 @@ public class FullscreenActivity extends AppCompatActivity {
                 mPlayer.start();
             } catch (Exception e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    private class DownloadTask extends AsyncTask<String, Integer, String> {
+
+        private Context context;
+        private PowerManager.WakeLock mWakeLock;
+
+        public DownloadTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... sUrl) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(sUrl[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                // expect HTTP 200 OK, so we don't mistakenly save error report
+                // instead of the file
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    return "Server returned HTTP " + connection.getResponseCode()
+                            + " " + connection.getResponseMessage();
+                }
+
+                // this will be useful to display download percentage
+                // might be -1: server did not report the length
+                int fileLength = connection.getContentLength();
+
+                // download the file
+                input = connection.getInputStream();
+                output = new FileOutputStream(context.getFilesDir().getAbsolutePath() + "/main.zip");
+
+                byte data[] = new byte[4096];
+                long total = 0;
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    // allow canceling with back button
+                    if (isCancelled()) {
+                        input.close();
+                        return null;
+                    }
+                    total += count;
+                    // publishing the progress....
+                    if (fileLength > 0) // only if total length is known
+                        publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
+            } catch (Exception e) {
+                return e.toString();
+            } finally {
+                try {
+                    if (output != null)
+                        output.close();
+                    if (input != null)
+                        input.close();
+                } catch (IOException ignored) {
+                }
+
+                if (connection != null)
+                    connection.disconnect();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String result) {
+            if (result != null){
+                Toast.makeText(context,"Download error: " + result, Toast.LENGTH_SHORT).show();
+            }
+            else{
+                TextView step1 = findViewById(R.id.step1);
+                step1.setTextColor(Green);
+                unpackZip(context.getFilesDir().getAbsolutePath() + "/", "main.zip");
             }
         }
     }
@@ -99,9 +256,80 @@ public class FullscreenActivity extends AppCompatActivity {
         }
     };
 
+    private void startServer(int port) {
+        try {
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
+            mHttpServer = HttpServer.create(inetSocketAddress, 0);
+            mHttpServer.setExecutor(Executors.newCachedThreadPool());
+            mHttpServer.createContext("/", rootHandler);
+            //mHttpServer.createContext("/index", rootHandler);
+            mHttpServer.start();//start server;
+            Log.d("--------> ", "Server is running on " + mHttpServer.getAddress() + ":" + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean unpackZip(String path, String zipname) {
+        InputStream is;
+        ZipInputStream zis;
+        try
+        {
+            String filename;
+            is = new FileInputStream(path + zipname);
+            zis = new ZipInputStream(new BufferedInputStream(is));
+            ZipEntry ze;
+            byte[] buffer = new byte[1024];
+            int count;
+
+            while ((ze = zis.getNextEntry()) != null)
+            {
+                filename = ze.getName();
+
+                // Need to create directories if not exists, or
+                // it will generate an Exception...
+                if (ze.isDirectory()) {
+                    File fmd = new File(path + filename);
+                    fmd.mkdirs();
+                    continue;
+                }
+
+                FileOutputStream fout = new FileOutputStream(path + filename);
+
+                while ((count = zis.read(buffer)) != -1)
+                {
+                    fout.write(buffer, 0, count);
+                }
+
+                fout.close();
+                zis.closeEntry();
+            }
+
+            zis.close();
+
+            TextView step2 = findViewById(R.id.step2);
+            step2.setTextColor(Green);
+
+            startServer(WebServerPort);
+
+            TextView step3 = findViewById(R.id.step3);
+            step3.setTextColor(Green);
+
+            LoadURL("http://localhost:" + WebServerPort + "/index.html");
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MyContext = this;
 
         binding = ActivityFullscreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -119,8 +347,6 @@ public class FullscreenActivity extends AppCompatActivity {
         });
 
         this.SettingsLayout = findViewById(R.id.settings);
-        TextInputLayout input = findViewById(R.id.input);
-        Button button = findViewById(R.id.button);
 
         mContentView = binding.fullscreenContent;
 
@@ -130,19 +356,17 @@ public class FullscreenActivity extends AppCompatActivity {
 
         sharedPref = getSharedPreferences("myPref", MODE_PRIVATE);
 
-        String url = sharedPref.getString("url", "null");
-        if(url != "null"){
-            LoadURL(url);
-        }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String value = input.getEditText().getText().toString();
-                sharedPref.edit().putString("url", value).commit();
-                LoadURL(value);
-            }
-        });
+        File file = new File(getFilesDir().getAbsolutePath() + "/main.zip");
+        if(!file.exists()){
+            final DownloadTask downloadTask = new DownloadTask(this);
+            downloadTask.execute("https://github.com/HeyHeyChicken/My-LaMetric-Time/archive/refs/heads/main.zip");
+        }
+        else{
+            TextView step1 = findViewById(R.id.step1);
+            step1.setTextColor(Green);
+            unpackZip(getFilesDir().getAbsolutePath() + "/", "main.zip");
+        }
     }
 
     @Override
@@ -165,9 +389,8 @@ public class FullscreenActivity extends AppCompatActivity {
     }
 
     private void LoadURL(String _url){
-        //Log.d("-------> LOADING", _url);
-        this.SettingsLayout.setVisibility(View.GONE);
         this.MyWebView.loadUrl(_url);
+        this.SettingsLayout.setVisibility(View.GONE);
     }
 
     /**
